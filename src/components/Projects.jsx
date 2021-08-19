10c718b7957b10fccb1d5a1ef044e51f4494ce15
SiamAnzir <siamanzir18@gmail.com>
2021-08-20 00:02:43 +0600

    [Modified] padding of component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -44,8 +44,8 @@ const Projects = () => {
                         }
                     </Row>
                 </div>
+                <br/>
             </Container>
-            <br/>
             <br/>
         </section>
     )
