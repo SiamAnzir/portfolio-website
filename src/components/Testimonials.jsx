5998b698be81e581393158a36a569a6251804222
SiamAnzir <siamanzir18@gmail.com>
2021-09-07 23:22:32 +0600

    [Added] Carousel & slideImg

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,42 +1,51 @@
 import React from "react";
-import {Card, Col, Container, Image, Row} from "react-bootstrap";
+import {Container, Image,Carousel} from "react-bootstrap";
 import {allTestimonials} from "../store/store";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faCertificate} from "@fortawesome/free-solid-svg-icons";
+import slideImg from "../assets/blackImage.png";
 
 const Testimonials = () => {
   return(
       <section id="testimonials" className="mt-5">
+          <br/>
           <Container>
               <div className="text-center">
                   <h2> <FontAwesomeIcon icon={faCertificate}/> <u> Client Testimonials </u></h2>
               </div>
-              <div className="col-xxl-8 col-xl-9 col-lg-12 col-md-6 mx-auto mt-5">
-                  <Row>
+              <div className="col-xxl-8 col-xl-9 col-lg-12 col-md-12 mx-auto mt-5">
+                  <Carousel variant="light">
                       {
                           allTestimonials.map((testimonial) => (
-                              <Col key={testimonial.id}>
-                                  <Card className="p-3" border="info" style={{width:'400px',height:'200px',background:'#323232',color:'white'}}>
+                              <Carousel.Item key={testimonial.id} interval={2000}>
+                                  <img
+                                      className="d-block w-100"
+                                      src={slideImg}
+                                      alt={testimonial.alt}
+                                      height={250}
+                                      width={500}
+                                  />
+                                  <Carousel.Caption>
                                       <blockquote className="blockquote mb-0 card-body">
                                           <p>
                                               {testimonial.quote}
                                           </p>
                                           <footer className="text-end blockquote-footer" style={{paddingTop:'.7rem'}}>
-                                              <Image src={testimonial.image} style={{height:'50px',width:'55px',marginLeft:'.3rem'}} roundedCircle/>
-                                              <small style={{marginLeft:'.5rem'}}>
+                                              <Image src={testimonial.image} style={{height:'60px',width:'65px',marginLeft:'.3rem'}} roundedCircle/>
+                                              <small style={{marginLeft:'.7rem',fontSize:'18px'}}>
                                                   {testimonial.name}
                                               </small>
                                           </footer>
                                       </blockquote>
-                                  </Card>
-                                  <br/>
-                              </Col>
+                                  </Carousel.Caption>
+                              </Carousel.Item>
                           ))
                       }
-                  </Row>
+                  </Carousel>
               </div>
               <br/>
           </Container>
+          <br/>
       </section>
   )
 };
