b7e252fece62c58bca3e9a5fb544d6b64aa74f3f
SiamAnzir <siamanzir18@gmail.com>
2022-06-01 22:20:47 +0600

    [Modified] about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {Col, Container, Row, Button, Image, Card} from "react-bootstrap";
 import ProfilePic from "../assets/ProfilePic.jpg";
 import emoji from "../assets/emoji.png";
+import Resume from "../assets/File/Mohammed Siam Anzir.pdf";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faFacebook, faGithub, faInstagram, faLinkedin} from "@fortawesome/free-brands-svg-icons";
 import {
@@ -43,15 +44,11 @@ const About = () => {
                                         <h2>I love to build amazing web apps.</h2>
                                     </Card.Title>
                                     <Card.Text className="mb-8 leading-relaxed" style={{minHeight:'185px'}}>
-                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-                                        laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-                                        Laborum, voluptas natus? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-                                        laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-                                        Laborum, voluptas natus? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-                                        laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-                                        Laborum, voluptas natus? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-                                        laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-                                        Laborum, voluptas natus?
+                                        It was really hard to carry motivation as a self-taught programmer. But love for coding, taking on new challenges and solving problems always push me up to my next milestone.
+                                        <br/>
+                                        Currently, I am working on React.js. Before that, I worked on HTML,CSS, JavaScript & Jquery and did some cool projects with that.
+                                        <br/>
+                                        As a self-taught developer and quick learner, I can easily cope up with any new tech stack to get the work done and take business to next level. I always have a soft corner for contributing to the tech community.
                                     </Card.Text>
                                     <div style={{paddingTop:'4rem'}}>
                                         <Row>
@@ -62,7 +59,10 @@ const About = () => {
                                                 <Button variant="outline-primary" href="#projects" className="responsiveBtn"><FontAwesomeIcon icon={faTasks} style={{marginRight:'.5rem'}}/>See My Past Works</Button>
                                             </Col>
                                             <Col xl={4} lg={4} md={4} sm={12}>
-                                                <Button variant="outline-primary" className="responsiveBtn"><FontAwesomeIcon icon={faDownload} style={{marginRight:'.5rem'}}/>Download Resume </Button>
+                                                <Button variant="outline-primary" className="responsiveBtn" href={Resume} download="Mohammed_Siam_Anzir.pdf">
+                                                    <FontAwesomeIcon icon={faDownload} style={{marginRight:'.5rem'}}/>
+                                                     Download Resume
+                                                </Button>
                                             </Col>
                                         </Row>
                                     </div>
